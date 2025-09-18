f38703be442296343eabbf2c4f7b7155ed7767ef
yijoo <761610@naver.com>
2025-09-18 16:57:05 +0900

    style: 채팅 메세지 시간 포멧 수정

--- src/main/java/likelion/be/areteum/chat/chat/dto/ChatDto.java
+++ src/main/java/likelion/be/areteum/chat/chat/dto/ChatDto.java
@@ -1,12 +1,17 @@
 package likelion.be.areteum.chat.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class ChatDto {
     private Long id;
     private String content;
-    private String createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd, hh:mm a", locale = "en_US")
+    private LocalDateTime createdAt;
 }
