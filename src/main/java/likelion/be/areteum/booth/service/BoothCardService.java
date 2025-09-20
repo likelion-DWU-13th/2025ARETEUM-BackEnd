707c9a3532b1891cb72d0a37cb2ae0fcacc4ed51
Heejin <hihj0914@gmail.com>
2025-09-20 23:35:22 +0900

    search ë 기능 ìpull 및 json 데이터 추가

--- src/main/java/likelion/be/areteum/booth/service/BoothCardService.java
+++ src/main/java/likelion/be/areteum/booth/service/BoothCardService.java
@@ -1,18 +1,17 @@
 package likelion.be.areteum.booth.service;
 
 import likelion.be.areteum.booth.dto.BoothCardRes;
+import likelion.be.areteum.booth.dto.BoothSearchRes;
 import likelion.be.areteum.booth.entity.BoothSchedule;
 import likelion.be.areteum.booth.entity.Category;
-import likelion.be.areteum.booth.entity.SubCategory;
 import likelion.be.areteum.booth.repository.BoothScheduleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,24 +20,26 @@ public class BoothCardService {
 
     private final BoothScheduleRepository scheduleRepo;
 
-    public Page<BoothCardRes> findCards(LocalDate date,
-                                        Category category,
-                                        SubCategory sub,
-                                        String query,
-                                        Pageable pageable) {
+    public BoothSearchRes findCards(LocalDate date,
+                                    Category category,
+                                    String query) {
         String q = (StringUtils.hasText(query) ? query : null);
 
-        Page<BoothSchedule> page = scheduleRepo.filterPaged(date, category, sub, q, pageable);
+        List<BoothSchedule> schedules = scheduleRepo.filterAll(date, category, q);
 
-        return page.map(s -> new BoothCardRes(
-                s.getBooth().getId(),
-                s.getBooth().getName(),
-                s.getBooth().getCategory(),
-                s.getBooth().getSubCategory(),
-                s.getBooth().getLocation(),
-                s.getEventDate(),
-                s.getStartTime(),
-                s.getEndTime()
-        ));
+        List<BoothCardRes> boothCards = schedules.stream()
+                .map(s -> new BoothCardRes(
+                        s.getBooth().getId(),
+                        s.getBooth().getName(),
+                        s.getBooth().getCategory(),
+                        s.getBooth().getSubCategory(),
+                        s.getBooth().getLocation(),
+                        s.getEventDate(),
+                        s.getStartTime(),
+                        s.getEndTime()
+                ))
+                .toList();
+
+        return new BoothSearchRes(boothCards, boothCards.size());
     }
 }
