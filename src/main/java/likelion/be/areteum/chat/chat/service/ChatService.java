c0ff5b42bc4948818dc0aa70ccf72b5a9bf3aaa0
yijoo <761610@naver.com>
2025-09-30 03:50:16 +0900

    fix:ì솜챗톡 수정

--- src/main/java/likelion/be/areteum/chat/chat/service/ChatService.java
+++ src/main/java/likelion/be/areteum/chat/chat/service/ChatService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,32 +18,31 @@ public class ChatService {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatRepository chatRepository;
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd, hh:mm a");
 
-    //채팅 저장
+    // 채팅 저장
     public void saveMessageAndSend(String clientId, String content) {
-        // 1. DB에 메시지 저장
         ChatEntity chatEntity = new ChatEntity();
         chatEntity.setClientId(clientId);
         chatEntity.setContent(content);
         chatEntity.setCreatedAt(LocalDateTime.now());
+
         chatRepository.save(chatEntity);
 
         ChatDto chatDto = convertToDto(chatEntity);
         messagingTemplate.convertAndSend("/topic/public", chatDto);
     }
 
-  //최근 100개 불러오기
+    // 최근 100개 메시지 조회
     public List<ChatDto> getRecentMessages() {
-        List<ChatEntity> messages = chatRepository.findTop100ByOrderByCreatedAtDescIdDesc();
+        List<ChatEntity> messages = chatRepository.findTop100ByOrderByCreatedAtDesc();
         return messages.stream()
                 .sorted(Comparator.comparing(ChatEntity::getCreatedAt)
-                        .thenComparing(ChatEntity::getId)) // 생성 시간 및 ID 오름차순(ASC)으로 정렬
+                        .thenComparing(ChatEntity::getId))
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
     }
 
-    // 엔티티를 dto로 변환
+    // 엔티티 → DTO 변환
     private ChatDto convertToDto(ChatEntity chatEntity){
         ChatDto dto = new ChatDto();
         dto.setId(chatEntity.getId());
