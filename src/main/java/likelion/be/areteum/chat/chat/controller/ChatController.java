fd5e5065f0a675f8699bdd8d145a260a6d878ed7
yijoo <761610@naver.com>
2025-09-30 02:42:57 +0900

    fix:솜챗

--- src/main/java/likelion/be/areteum/chat/chat/controller/ChatController.java
+++ src/main/java/likelion/be/areteum/chat/chat/controller/ChatController.java
@@ -6,11 +6,9 @@ import likelion.be.areteum.chat.chat.dto.ChatDto;
 import likelion.be.areteum.chat.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,18 +16,13 @@ import java.util.List;
 @Tag(name="Chat API", description = "채팅 API - 담당(이주연, 최이주)")
 public class ChatController {
     private final ChatService chatService;
-    private final SimpMessagingTemplate messagingTemplate;
 
-    // '/app/chat'
     @MessageMapping("/chat") //send destination
     public void sendMessage(ChatDto message){
-        chatService.saveMessage(message.getClientId(), message.getContent());
-        message.setCreatedAt(LocalDateTime.now());
-        //메시지 전달
-        messagingTemplate.convertAndSend("/topic/chat", message);
+        chatService.saveMessageAndSend(message.getClientId(), message.getContent());
     }
 
-    //최근 100개 불러오기
+    // 최근 100개 불러오기 (REST API 호출)
     @GetMapping("/chat/history")
     @Operation(summary = "최근 100개 채팅 불러오기")
     public List<ChatDto> getRecentMessage(){
